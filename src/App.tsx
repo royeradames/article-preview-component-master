6b0486b80cb5cba6720c92329f10b565fcc96505
royeradames <royeraadames@gmail.com>
2021-03-28 19:48:42 -0400

    📦style footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,46 @@ import React from "react";
 import "./App.scss";
 import drawers from "./images/drawers.jpg";
 import authorImage from "./images/avatar-michelle.jpg";
+import shareIcon from "./images/icon-share.svg";
 
 function App() {
   return (
     <>
       <main className="page">
         <article className="article">
-          <header className="article__header">
-            <img src={drawers} alt=" Drawers" />
-          </header>
-          <h2 className="article__title">
-            Shift the overall look and feel by adding these wonderful touches to
-            furniture in your home
-          </h2>
+          <img
+            src={drawers}
+            alt=" Drawers"
+            className="article__heading-image"
+          />
 
-          <p className="article__message">
-            Ever been in a room and felt like something was missing? Perhaps it
-            felt slightly bare and uninviting. I’ve got some simple tips to help
-            you make any room feel complete.
-          </p>
-          <footer className="article__footer">
-            <div className="article__selfie">
-              <img src={authorImage} alt="Selfie of Michelle Appleton" />
-            </div>
-            <h3 className="article__author_name">Michelle Appleton</h3>
-            <p className="article__publish_date">28 Jun 2020</p>
-            <button className="article__share">Share</button>
-          </footer>
+          <div className="article_inner">
+            <h2 className="article__title">
+              Shift the overall look and feel by adding these wonderful touches
+              to furniture in your home
+            </h2>
+            <p className="article__message">
+              Ever been in a room and felt like something was missing? Perhaps
+              it felt slightly bare and uninviting. I’ve got some simple tips to
+              help you make any room feel complete.
+            </p>
+            <footer className="article__footer">
+              <div>
+                <img
+                  className="article__selfie"
+                  src={authorImage}
+                  alt="Selfie of Michelle Appleton"
+                />
+                <div className="article__info">
+                  <h3 className="article__author_name">Michelle Appleton</h3>
+                  <p className="article__publish_date">28 Jun 2020</p>
+                </div>
+              </div>
+              <button className="article__share">
+                <img src={shareIcon} alt="share" />
+              </button>
+            </footer>
+          </div>
         </article>
       </main>
     </>
