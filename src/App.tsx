7d929f36eccb707319cc2c6dc54e8ad29353eed6
royeradames <royeraadames@gmail.com>
2021-03-30 12:05:44 -0400

    👌up to day

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import "./App.scss";
 import drawers from "./images/drawers.jpg";
 import authorImage from "./images/avatar-michelle.jpg";
 import shareIcon from "./images/icon-share.svg";
+import twitterIcon from "./images/icon-twitter.svg";
+import facebookIcon from "./images/icon-facebook.svg";
+import pinterestIcon from "./images/icon-pinterest.svg";
 
 function App() {
+  const share = () => {};
   return (
     <>
       <main className="page">
@@ -25,23 +29,47 @@ function App() {
               it felt slightly bare and uninviting. I’ve got some simple tips to
               help you make any room feel complete.
             </p>
-            <footer className="article__footer">
-              <div>
-                <img
-                  className="article__selfie"
-                  src={authorImage}
-                  alt="Selfie of Michelle Appleton"
-                />
-                <div className="article__info">
-                  <h3 className="article__author_name">Michelle Appleton</h3>
-                  <p className="article__publish_date">28 Jun 2020</p>
-                </div>
-              </div>
-              <button className="article__share">
-                <img src={shareIcon} alt="share" />
-              </button>
-            </footer>
           </div>
+
+          <footer className="article__footer">
+            <div className="article__share-section display-none">
+              <h2 className="article__share-title">SHARE</h2>
+              <img
+                src={facebookIcon}
+                alt=" facebook icon"
+                className="article__facebook-icon"
+              />
+              <img
+                src={twitterIcon}
+                alt="twitter icon"
+                className="article__facebook-icon"
+              />
+              <img
+                src={pinterestIcon}
+                alt="pinterest icon"
+                className="article__pinterest"
+              />
+            </div>
+            <div className="article__author">
+              <img
+                className="article__selfie"
+                src={authorImage}
+                alt="Selfie of Michelle Appleton"
+              />
+              <div className="article__info">
+                <h3 className="article__author_name">Michelle Appleton</h3>
+                <p className="article__publish_date">28 Jun 2020</p>
+              </div>
+            </div>
+            <button onClick={() => share()} className="article__share-button">
+              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="13">
+                <path
+                  fill="#6E8098"
+                  d="M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z"
+                />
+              </svg>
+            </button>
+          </footer>
         </article>
       </main>
     </>
